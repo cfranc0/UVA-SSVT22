5e3a29f16eada922413530a8076824e43f39a121
Chronis Kontomaris <xmd@protonmail.com>
2022-09-09 11:23:22 +0200

    nthn major

--- _ChronisKontomaris/Exercise8.hs
+++ _ChronisKontomaris/Exercise8.hs
@@ -6,15 +6,15 @@ boys = [Matthew, Peter, Jack, Arnold, Carl]
 --function declaration for when a boy accuses onother one 
 
 accuses ::  Boy -> Boy -> Bool
---Implementation of statement like "Mathew says Carl dont do it and neither did he"
+--Implementation of statement  "Mathew says Carl dont do it and neither did he"
 accuses Matthew name = not (name==Carl) && not (name==Matthew) 
---Implementation of statement like "Peter: It was Matthew or it was Jack.
+--Implementation of statement  "Peter: It was Matthew or it was Jack.
 accuses  Peter name =name == Jack || name== Matthew
---Implementation of statement like "Jack: Matthew and Peter are both lying"
+--Implementation of statement  "Jack: Matthew and Peter are both lying"
 accuses Jack name =not (accuses (Jack)name) && not (accuses(Matthew) name)
---Implementation of statement like "Arnold: Matthew or Peter is speaking the truth, but not both."
+--Implementation of statement  "Arnold: Matthew or Peter is speaking the truth, but not both."
 accuses Arnold name = (name==Jack || name==Peter) && not(accuses (Jack)name)  && (accuses(Matthew) name)
---Implementation of statement like "Carl: What Arnold says is not true"
+--Implementation of statement  "Carl: What Arnold says is not true"
 accuses Carl name= not(accuses(Arnold) name)
 
 
