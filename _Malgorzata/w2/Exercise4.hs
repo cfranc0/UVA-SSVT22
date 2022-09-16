6cca4099003714e00efab9d0b04460cb8003ec00
Chronis Kontomaris <xmd@protonmail.com>
2022-09-16 10:25:33 +0200

    property test

--- _Malgorzata/w2/Exercise4.hs
+++ _Malgorzata/w2/Exercise4.hs
@@ -3,6 +3,9 @@ import Data.List
 isPermutation :: Eq a => [a] -> [a] -> Bool
 isPermutation a b = length a == length b && all (`elem` b) a
 
+--property_reverse :: Eq a => [a] -> [a] -> Bool
+--property_reverse a b = isPermutation a b && isPermutation (reverse a) b && isPermutation  a (reverse b) && isPermutation  (reverse a) (reverse b)
+
 {- to recognize the permutation of the list, there are few conditions that needs to be met
 both lists need to have the same lenght, the same elements, and the same sum.
 However, if we can assume, that the lists do not contain duplicates, we don't need to check the sum.
