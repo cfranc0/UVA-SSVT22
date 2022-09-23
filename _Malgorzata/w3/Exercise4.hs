b51450fc8f0c1d61f4e798c4bddb11b61bf4a7e3
asirre <goss.janeczko@gmail.com>
2022-09-23 15:08:53 +0200

    4 and 5

---
+++ _Malgorzata/w3/Exercise4.hs
@@ -0,0 +1,26 @@
+module Exercise4 where
+import Lecture3
+import Test.QuickCheck
+import Control.Monad
+import SetOrd
+
+instance Arbitrary Form where
+    arbitrary = Prop <$> choose (1,9)
+
+genForm = sized genForm'
+
+genForm' :: Int -> Gen Form
+genForm' 0 = arbitrary
+genForm' n | n>0 =
+            oneof  [arbitrary,
+            liftM2 twoArgCnj subGenForm subGenForm,
+            liftM2 twoArgDsj subGenForm subGenForm,
+            liftM2 Equiv subGenForm subGenForm,
+            liftM2 Impl subGenForm subGenForm]
+    where subGenForm = genForm' (n `div` 2)
+
+twoArgCnj :: Form -> Form -> Form
+twoArgCnj x y = Cnj [x, y]
+
+twoArgDsj :: Form -> Form -> Form
+twoArgDsj x y = Dsj [x, y]
---
+++ _Malgorzata/w3/Exercise5.hs
@@ -0,0 +1,55 @@
+module Exercise5 where
+
+import SetOrd
+import Lecture3
+import Test.QuickCheck
+import Data.List
+import Test.QuickCheck (Positive, Property)
+
+
+sub :: Form -> Set Form
+sub (Prop x) = Set [Prop x]
+sub (Neg f) = unionSet (Set [Neg f]) (sub f)
+sub f@(Cnj [f1,f2]) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
+sub f@(Dsj [f1,f2]) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
+sub f@(Impl f1 f2) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
+sub f@(Equiv f1 f2) = unionSet ( unionSet (Set [f]) (sub f1)) (sub f2)
+
+
+-- The set of the formula consisting one atom is a set of this atom
+propSetTest :: Int -> Bool
+propSetTest x = sub (Prop x) == Set [Prop x]
+
+--
+-- Testing whether the sub-formulas for formula -p ^ p is {p, Neg p, -p Cnj p}
+propFormTest :: Int -> Int -> Property
+propFormTest x y = (x >=0 && y >= 0) ==> set1 `subSet` set2 && set2 `subSet` set1
+    where
+    px = Prop x
+    py = Prop y
+    set1 = sub (Cnj [Neg px, Neg py])
+    set2 =  Set (sort (nub [px, py, Neg px, Neg py, Cnj [Neg px, Neg py]]))
+
+
+{- 5.1 
+Time spent 2,5h 
+The implementation is hard to prove. We would have to know all the
+sub-formulas for the given formula. 
+One way we could do it is to implement a generator that 
+generates a formula, and takes track of the sub-formulas used to create that
+formula. This generator can be made using recursive approach, but would require some 
+constraints like a maximum depth of the recursion tree.
+-}
+
+
+nsub' :: Set Form -> Int -> Int
+nsub' (Set []) n = n
+nsub' (Set (x:xs)) n = nsub' (Set xs) (n+1)
+
+nsub :: Form -> Int
+nsub f = nsub' (sub f) 0
+
+
+
+
+
