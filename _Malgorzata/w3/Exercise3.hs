30c09e668a188dbd7ccc7e878a596cfdf7f8179f
asirre <goss.janeczko@gmail.com>
2022-09-23 19:36:36 +0200

    desc for 3

--- _Malgorzata/w3/Exercise3.hs
+++ _Malgorzata/w3/Exercise3.hs
@@ -14,3 +14,13 @@ distribute f = f
 
 cnf :: Form -> Form
 cnf f = distribute (nnf (arrowfree f))
+
+{-
+Firstly, if there is implification or equality symbol in formula
+we apply arrowfree function that exchanges it to dsj or cnj.
+Then we apply nnf function on the result that converts negation of dsj or cnj
+to negations of props only, repeatedly using De'Morgans laws, example:
+~(B v C) to (~B) ^ (~C)
+Finally, distribute function changes nested cnj within dsj, example:
+A ^ (B v (D ^ E)) to (A) ^ (B v D) ^ (B v E)
+-}
