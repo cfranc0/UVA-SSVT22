d67da8a896fc530cd5a3c6ceb34c6b7e4fe9e5bd
Chronis Kontomaris <xmd@protonmail.com>
2022-09-29 09:09:26 +0200

    exercise1

--- _ChronisKontomaris/Week3/Exercise6.hs
+++ _ChronisKontomaris/Week3/Exercise6.hs
@@ -1,7 +1,7 @@
 -- Time spent: 180 minutes --
 
 
-import Lecture3
+import Lecture3 ( parse, Form(Dsj, Cnj, Neg, Prop) )
 
 
 type Clause=[Int]
---
+++ _ChronisKontomaris/Week4/Exercise1.hs
@@ -0,0 +1,8 @@
+import LTS
+
+validateLTS :: LTS -> Bool
+validateLTS ([],_, _, _)=False
+validateLTS (states, labels, transitionSet, initState) = (length states) < (maxBound :: Int) && (length labels) < (maxBound :: Int) && elem initState states
+
+validateIOLTS :: IOLTS->Bool
+validateIOLTS (states, labels1,labels2, transitionSet, initState)=validateLTS(states,labels1++labels2,transitionSet,initState) && (filter(\x -> elem x labels2) labels1==[])
